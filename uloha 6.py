68af00a1d990a71a1eb7d89939c44448375c5a14
AjkaKrilek <113473323+AjkaKrilek@users.noreply.github.com>
2022-09-20 21:45:21 +0200

    Add files via upload

---
+++ uloha 6.py
@@ -0,0 +1,5 @@
+a=int(input('zadaj mi cislo:'))
+if a%2==0:
+    print('je parne')
+else:
+    print('nie je parne')
---
+++ uloha 7.py
@@ -0,0 +1,7 @@
+a=int(input('zadaj cislo:'))
+if a==0:
+    print('rovna sa nule')
+elif a%2==0:
+    print('je parne')
+else:
+    print('je neparne')
